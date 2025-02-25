9c28dec2694aaceb4addecb76d0f0d72bb7f7274
trongtoannguyen <trongtoan1609ht@gmail.com>
2025-02-26 01:07:05 +0700

    add Swift app supports ios device

---
+++ ReddotMobile/ReddotMobile/ReddotMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReddotMobileApp.swift
+//  ReddotMobile
+//
+//  Created by Nguyen Trong Toan on 17/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ReddotMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
